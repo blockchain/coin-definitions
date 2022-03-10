c1001383b32a1cd2f08eb50662f9b2ec5c4946e2
Pablo Jorge <pjorge@blockchain.com>
2022-03-10 12:12:04 -0300

    Generalize tokens file generator

--- scripts/build-lists.py
+++ scripts/build-lists.py
@@ -103,12 +103,13 @@ class Blockchain:
         dict_.update(dict(key=key))
         return build_dataclass_from_dict(cls, dict_)
 
-
-# ERC-20 params
-ETH_ASSETS = "assets/blockchains/ethereum/assets/"
-
-ETH_EXT_ASSETS = "extensions/blockchains/ethereum/assets/"
-ETH_EXT_ASSETS_DENYLIST = "extensions/blockchains/ethereum/denylist.txt"
+@dataclass
+class ERC20Network:
+    name: str
+    assets_dir: str
+    ext_assets_dir: str
+    denylist: str
+    output_file: str
 
 TW_REPO_ROOT = "https://raw.githubusercontent.com/trustwallet/assets/master/"
 BC_REPO_ROOT = "https://raw.githubusercontent.com/blockchain/coin-definitions/master/"
@@ -122,7 +123,16 @@ EXT_BLOCKCHAINS_DENYLIST = "extensions/blockchains/denylist.txt"
 EXT_PRICES = "extensions/prices.json"
 
 FINAL_BLOCKCHAINS_LIST="coins.json"
-FINAL_ERC20_TOKENS_LIST="erc20-tokens.json"
+
+ERC20_NETWORKS = [
+    ERC20Network(
+        name="ethereum",
+        assets_dir="assets/blockchains/ethereum/assets/",
+        ext_assets_dir="extensions/blockchains/ethereum/assets/",
+        denylist="extensions/blockchains/ethereum/denylist.txt",
+        output_file="erc20-tokens.json"
+    )
+]
 
 def chunks(lst, n):
     for i in range(0, len(lst), n):
@@ -252,10 +262,10 @@ def fetch_coins():
 
     return list(coins)
 
-def fetch_erc20_tokens():
+def fetch_erc20_tokens(assets_dir):
     # Fetch and parse all info.json files:
-    print(f"Reading ETH assets from {ETH_ASSETS}")
-    assets = [Asset.from_dict(info) for key, info in read_assets(ETH_ASSETS)]
+    print(f"Reading ETH assets from {assets_dir}")
+    assets = [Asset.from_dict(info) for key, info in read_assets(assets_dir)]
 
     # Keep only the active ones:
     assets = filter(lambda x: x.status == 'active', assets)
@@ -265,9 +275,9 @@ def fetch_erc20_tokens():
 
     return list(tokens)
 
-def fetch_prices():
+def fetch_prices(assets_dir):
     coins = fetch_coins()
-    tokens = fetch_erc20_tokens()
+    tokens = fetch_erc20_tokens(assets_dir)
 
     symbols = list(set([c.symbol for c in coins] + [t.symbol for t in tokens]))
 
@@ -291,8 +301,9 @@ def build_coins_list():
     print(f"Writing {len(coins)} coins to {FINAL_BLOCKCHAINS_LIST}")
     write_json(coins, FINAL_BLOCKCHAINS_LIST, sort_keys=False, indent=2)
 
-def build_erc20_tokens_list():
-    tokens = fetch_erc20_tokens()
+def build_erc20_tokens_list(erc20_network):
+    print(f"Generating token files for network \"{erc20_network.name}\"")
+    tokens = fetch_erc20_tokens(erc20_network.assets_dir)
 
     print(f"Reading ETH asset prices from {EXT_PRICES}")
     prices = read_json(EXT_PRICES)
@@ -301,20 +312,20 @@ def build_erc20_tokens_list():
     tokens = list(filter(lambda token: token.symbol in prices['prices'], tokens))
 
     # Include already selected tokens (to make sure we don't remove a token we had previously selected)
-    print(f"Reading existing assets in {FINAL_ERC20_TOKENS_LIST}")
-    current_tokens = list(map(lambda x: ERC20Token(**x), read_json(FINAL_ERC20_TOKENS_LIST)))
+    print(f"Reading existing assets in {erc20_network.output_file}")
+    current_tokens = list(map(lambda x: ERC20Token(**x), read_json(erc20_network.output_file)))
     tokens = list(set(tokens) | set(current_tokens))
 
     # Make sure the asset is NOT in the denylist:
-    bc_denylist = set(map(lambda x: x.lower(), read_txt(ETH_EXT_ASSETS_DENYLIST)))
+    bc_denylist = set(map(lambda x: x.lower(), read_txt(erc20_network.denylist)))
     tokens = filter(lambda x: x.address.lower() not in bc_denylist, tokens)
 
     # Make sure the asset is valid:
     tokens = filter(lambda x: x.is_valid(), tokens)
 
     # Merge with extensions:
-    print(f"Reading ETH asset extensions from {ETH_EXT_ASSETS}")
-    extensions = [Asset.from_dict(info) for key, info in read_assets(ETH_EXT_ASSETS)]
+    print(f"Reading ETH asset extensions from {erc20_network.ext_assets_dir}")
+    extensions = [Asset.from_dict(info) for key, info in read_assets(erc20_network.ext_assets_dir)]
     extensions = map(ERC20Token.from_asset, extensions)
     tokens = sorted(set(extensions) | set(tokens), key=lambda t: t.address)
 
@@ -328,8 +339,8 @@ def build_erc20_tokens_list():
     # Convert back to plain dicts:
     tokens = list(map(asdict, tokens))
 
-    print(f"Writing {len(tokens)} tokens to {FINAL_ERC20_TOKENS_LIST}")
-    write_json(tokens, FINAL_ERC20_TOKENS_LIST)
+    print(f"Writing {len(tokens)} tokens to {erc20_network.output_file}")
+    write_json(tokens, erc20_network.output_file)
 
 def build_custom_assets(chain, assets_dir, output_file):
     print(f"Reading custom asset from {assets_dir}")
@@ -354,10 +365,11 @@ def main():
     args = parser.parse_args()
 
     if args.fetch_prices:
-        fetch_prices()
+        fetch_prices(ERC20_NETWORKS[0].assets_dir)
     else:
         build_coins_list()
-        build_erc20_tokens_list()
+        for erc20_network in ERC20_NETWORKS:
+            build_erc20_tokens_list(erc20_network)
         build_custom_chain_lists()
 
 
