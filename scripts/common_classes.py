934751802fe0289dced8aafe149ede68bc0e5339
Aric <103421905+akuter-bc@users.noreply.github.com>
2023-10-09 13:34:21 +0200

    Update scripts/common_classes.py
    Co-authored-by: Jordan Huntbach <102958690+jhuntbach-bc@users.noreply.github.com>

--- scripts/common_classes.py
+++ scripts/common_classes.py
@@ -103,7 +103,7 @@ class ERC20Token:
     website: str
 
     def is_valid(self):
-        # At most 6 letters and digits:
+        # At most 8 letters and digits:
         return re.match("^[a-zA-Z0-9]{1,8}$", self.symbol) is not None
 
     @staticmethod
