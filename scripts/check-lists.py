153ff7eac48c9e38feeedebf75c4c1d3ecb03f84
jakubdziworski <jdziworski@blockchain.com>
2024-11-28 15:18:41 +0100

    feat(FC-309): Added BRETT, DEGEN, GOAT, GIGA, PNUT, BILLY tokens

--- scripts/check-lists.py
+++ scripts/check-lists.py
@@ -205,6 +205,8 @@ def check_currencies(custody_currencies, coins, eth_erc20_tokens, chains, prices
             ref = chains.get("CELO").get(currency.symbol)
         elif currency.type == "SOLANA_TOKEN":
             ref = chains.get("SOL").get(currency.symbol)
+        elif currency.type == "BASE_TOKEN":
+            ref = chains.get("BASEETH").get(currency.symbol)
         else:
             yield Error(currency, "Invalid type")
             continue
